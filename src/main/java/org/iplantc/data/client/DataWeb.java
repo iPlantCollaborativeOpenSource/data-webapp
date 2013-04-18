b9a752090bbe82594df86da4030a95788f60af04
Sriram Srinivasan <sriram@iplantcollaborative.org>
2013-04-17 17:04:30 -0700

    initial commit

---
+++ src/main/java/org/iplantc/data/client/DataWeb.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.iplantc.data.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author sriram
+ *
+ */
+public class DataWeb implements EntryPoint {
+
+    /* (non-Javadoc)
+     * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+     */
+    @Override
+    public void onModuleLoad() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
